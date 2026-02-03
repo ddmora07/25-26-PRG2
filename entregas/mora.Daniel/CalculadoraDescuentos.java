35f848febb82b3287105495d6e6594e65a1eaa75
ddmora07 <daniel.mora@alumnos.uneatlantico.es>
2026-02-03 13:53:02 +0100

    Lo primero visible de este código Del Calculadora Descuentos esque todo está en el main y debería separase por metodo

--- entregas/mora.Daniel/CalculadoraDescuentos.java
+++ entregas/mora.Daniel/CalculadoraDescuentos.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class CalculadoraDescuentos {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        // todo main
 
         System.out.println("Calculadora de Descuentos - Tienda Online");
 
