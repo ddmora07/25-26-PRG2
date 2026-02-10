5c8ca1b91ab0e1bbccec9094ac6708a2ab2d0dc0
Danielmora <daniel.mora@alumnos.uneatlantico.es>
2026-02-10 13:03:47 +0100

    hemos creado el proyecto y creado la variable estática de el número máximo de líneas y la entrada que es el scanner

---
+++ entregas/moraDaniel/MiPropioEdlin.java
@@ -0,0 +1,13 @@
+package entregas.moraDaniel;
+
+import java.util.Scanner;
+
+public class MiPropioEdlin {
+    static final int MAX_LINEAS = 10;
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+    }
+
+}
