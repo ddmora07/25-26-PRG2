de669cbb5a11be6d78ec5cd8dca6829607a60178
Danielmora <daniel.mora@alumnos.uneatlantico.es>
2026-02-17 13:10:46 +0100

    fix : corregir lógica de las minas . como equals las filas y columnas para entender mejor el código

--- entregas/moraDaniel/BuscaMinasCorregido.java
+++ entregas/moraDaniel/BuscaMinasCorregido.java
@@ -38,61 +38,56 @@ public class BuscaMinasCorregido {
 		///////////////////////////
 		int minasColocadas = 0;
 		while (minasColocadas < 5) {
-			int filaAleatorinaDeMina = (int) (Math.random() * 2 + 1);
-			int columnaAleatoriaDeMina = (int) (Math.random() * 4 + 1);
-			if (tableroMinasActivas[columnaAleatoriaDeMina][filaAleatorinaDeMina] == "1") {
-				minasColocadas--;
-			} else {
-				tableroMinasActivas[columnaAleatoriaDeMina + 1][filaAleatorinaDeMina + 1] = "1";
+			int filaAleatorinaDeMina = (int) (Math.random() * 5 + 1);
+			int columnaAleatoriaDeMina = (int) (Math.random() * 7 + 1);
+			if (!tableroMinasActivas[filaAleatorinaDeMina][columnaAleatoriaDeMina].equals("1")) {
+
+				tableroMinasActivas[filaAleatorinaDeMina][columnaAleatoriaDeMina] = "1";
+				minasColocadas++;
 			}
-			minasColocadas++;
 		}
 
 		int columnaMina = 0, filaMina = 0, juegoEncendido = 1, valoresCorrectos, contadorMapa = 0, contadorMinas = 0;
 		///////////// Comienza el juego
 		while (juegoEncendido == 1) {
 			for (minasColocadas = 0; minasColocadas < tableroVisible.length; minasColocadas++) {
-				for (int j = 0; j <= tableroVisible.length + 1; j++) {
+				for (int j = 0; j < tableroVisible[minasColocadas].length; j++)
 					System.out.print(tableroVisible[minasColocadas][j]);
-				}
-				System.out.println(" ");
 			}
-			valoresCorrectos = 0;// reseteo de la variable
-			while (valoresCorrectos == 0) {
-				System.out.println(" ");
-				System.out.println("Ingrese X");
-				filaMina = scanner.nextInt();
-				System.out.println("Ingrese Y");
-				columnaMina = scanner.nextInt();
-				if (columnaMina > 5) {
-					valoresCorrectos = 0;
-				} else if (filaMina > 7) {
-					valoresCorrectos = 0;
-				} else {
-					valoresCorrectos = 1;
-				}
-			}
-
-			if (tableroMinasActivas[columnaMina][filaMina] == "1") {
-				tableroVisible[columnaMina][filaMina] = "x";
-				contadorMinas++;
-			} else if (tableroMinasActivas[columnaMina][filaMina] == "0") {
-				tableroVisible[columnaMina][filaMina] = ".";
+			System.out.println(" ");
+		}
+		valoresCorrectos = 0;// reseteo de la variable
+		while (valoresCorrectos == 0) {
+			System.out.println(" ");
+			System.out.println("Ingrese X");
+			filaMina = scanner.nextInt();
+			System.out.println("Ingrese Y");
+			columnaMina = scanner.nextInt();
+			if (filaMina < 1 || filaMina > 5 || columnaMina < 1 || columnaMina > 7) {
+				valoresCorrectos = 0;
 			} else {
-				System.out.println("No es correcta esa opcion.");
-			}
-
-			contadorMapa++;
-			if (contadorMinas > 2) {
-				juegoEncendido = 0;
-				System.out.println("Has perdido");
-			} else if (contadorMapa >= 31) {
-				System.out.println("Felicidades Ganador!");
-				juegoEncendido = 0;
-			} else {
-
+				valoresCorrectos = 1;
 			}
 		}
-	}
 
+		if (tableroMinasActivas[filaMina][columnaMina].equals("1")) {
+			tableroVisible[filaMina][columnaMina] = "x";
+			contadorMinas++;
+		} else if (tableroMinasActivas[filaMina][columnaMina].equals("0")) {
+			tableroVisible[filaMina][columnaMina] = ".";
+		} else {
+			System.out.println("No es correcta esa opcion.");
+		}
+
+		contadorMapa++;
+		if (contadorMinas > 2) {
+			juegoEncendido = 0;
+			System.out.println("Has perdido");
+		} else if (contadorMapa >= 30) {
+			System.out.println("Felicidades Ganador!");
+			juegoEncendido = 0;
+		} else {
+
+		}
+	}
 }
