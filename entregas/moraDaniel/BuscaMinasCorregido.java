42d3afa6e2f0bac3d94ab7ea60b50ee50aedb4d6
Danielmora <daniel.mora@alumnos.uneatlantico.es>
2026-02-17 13:30:15 +0100

    refactor : Eliminación de comentarios

--- entregas/moraDaniel/BuscaMinasCorregido.java
+++ entregas/moraDaniel/BuscaMinasCorregido.java
@@ -4,38 +4,37 @@ import java.util.Scanner;
 
 public class BuscaMinasCorregido {
 
-	static String[][] tableroVisible = { // [0][0] [0][1] [0][2] [0][3] [0][4] [0][5] [0][6] [0][7]
+	static String[][] tableroVisible = {
 			{ " ", "1", "2", "3", "4", "5", "6", "7" },
-			// [1][0] [1][1] [1][2] [1][3] [1][4] [1][5] [1][6] [1][7]
+
 			{ "1", "-", "-", "-", "-", "-", "-", "-" },
-			// [2][0] [2][1] [2][2] [2][3] [2][4] [2][5] [2][6] [2][7]
+
 			{ "2", "-", "-", "-", "-", "-", "-", "-" },
-			// [3][0] [3][1] [3][2] [3][3] [3][4] [3][5] [3][6] [3][7]
+
 			{ "3", "-", "-", "-", "-", "-", "-", "-" },
-			// [4][0] [4][1] [4][2] [4][3] [4][4] [4][5] [4][6] [4][7]
+
 			{ "4", "-", "-", "-", "-", "-", "-", "-" },
-			// [5][0] [5][1] [5][2] [5][3] [5][4] [5][5] [5][6] [5][7]
+
 			{ "5", "-", "-", "-", "-", "-", "-", "-" }
 	};
 
-	static String[][] tableroMinasActivas = { // [0][0] [0][1] [0][2] [0][3] [0][4] [0][5] [0][6] [0][7]
+	static String[][] tableroMinasActivas = {
 			{ " ", "1", "2", "3", "4", "5", "6", "7" },
-			// [1][0] [1][1] [1][2] [1][3] [1][4] [1][5] [1][6] [1][7]
+
 			{ "1", "0", "0", "0", "0", "0", "0", "0" },
-			// [2][0] [2][1] [2][2] [2][3] [2][4] [2][5] [2][6] [2][7]
+
 			{ "2", "0", "0", "0", "0", "0", "0", "0" },
-			// [3][0] [3][1] [3][2] [3][3] [3][4] [3][5] [3][6] [3][7]
+
 			{ "3", "0", "0", "0", "0", "0", "0", "0" },
-			// [4][0] [4][1] [4][2] [4][3] [4][4] [4][5] [4][6] [4][7]
+
 			{ "4", "0", "0", "0", "0", "0", "0", "0" },
-			// [5][0] [5][1] [5][2] [5][3] [5][4] [5][5] [5][6] [5][7]
+
 			{ "5", "0", "0", "0", "0", "0", "0", "0" }
 	};
 
-	// Posiciones Random de la mina
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
-		///////////////////////////
+
 		int minasColocadas = 0;
 		while (minasColocadas < 5) {
 			int filaAleatorinaDeMina = (int) (Math.random() * 5 + 1);
@@ -48,7 +47,7 @@ public class BuscaMinasCorregido {
 		}
 
 		int columnaMina = 0, filaMina = 0, juegoEncendido = 1, valoresCorrectos, contadorMapa = 0, contadorMinas = 0;
-		///////////// Comienza el juego
+
 		while (juegoEncendido == 1) {
 			for (minasColocadas = 0; minasColocadas < tableroVisible.length; minasColocadas++) {
 				for (int j = 0; j < tableroVisible[minasColocadas].length; j++) {
@@ -58,7 +57,7 @@ public class BuscaMinasCorregido {
 				System.out.println(" ");
 
 			}
-			valoresCorrectos = 0;// reseteo de la variable
+			valoresCorrectos = 0;
 			while (valoresCorrectos == 0) {
 				System.out.println(" ");
 				System.out.println("Ingrese X");
